f41af7ada3406ea7099cf09aa4a1e34cc20ce560
Tim Taplin <taplin@Tims-MacBook-Pro.local>
2017-07-08 18:25:00 -0700

    first commit, needs switch to mysql

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // Date: 2017-06-07 23:47:43 +0000
 import PackageDescription
 let package = Package(
-	name: "PerfectLocalAuthPostgreSQLTemplate",
+	name: "NE1",
 	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
